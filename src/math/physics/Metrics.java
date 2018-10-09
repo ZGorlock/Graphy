13dbf1bcefa245345cb1c60768c1dd80f2ec6004
Zachary Gill <ZGorlock@gmail.com>
2018-10-08 21:37:30 -0400

    Added capability to determine volumes for Polyhedra

---
+++ src/math/physics/Metrics.java
@@ -0,0 +1,187 @@
+/*
+ * File:    Metrics.java
+ * Package: math.physics
+ * Author:  Zachary Gill
+ */
+
+package math.physics;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import math.vector.Vector;
+import objects.base.ObjectInterface;
+import objects.base.polygon.Polygon;
+import objects.polyhedron.Polyhedron;
+
+/**
+ * Contains physical metrics about a Polyhedron. 
+ */
+public class Metrics {
+    
+    //Constants
+    
+    /**
+     * The sample set to use for approximating the volume of a Polyhedron.
+     */
+    private static long SAMPLE_SET_FOR_VOLUME_APPROXIMATION = 10000; 
+    
+    
+    //Fields
+    
+    /**
+     * The Polyhedron these metrics refer to.
+     */
+    private Polyhedron polyhedron;
+    
+    /**
+     * The volume of the Polyhedron.
+     */
+    protected double volume;
+    
+    /**
+     * The density of the Polyhedron.
+     */
+    protected double density = 1.0;
+    
+    
+    //Constructors
+    
+    /**
+     * Constructor for the Metrics of a Polyhedron.
+     *
+     * @param polyhedron The Polyhedron to construct the Metrics for.
+     */
+    public Metrics(Polyhedron polyhedron) {
+        this.polyhedron = polyhedron;
+        calculateVolume();
+    }
+    
+    
+    //Methods
+    
+    /**
+     * Calculates the volume of the Polyhedron.
+     */
+    private void calculateVolume() {
+        Vector b = determineBoundingCube();
+        long within = 0L;
+        for (long i = 0; i < SAMPLE_SET_FOR_VOLUME_APPROXIMATION; i++) {
+            Vector p = new Vector(
+                    Math.random() * (b.get(1) - b.get(0)) + b.get(0),
+                    Math.random() * (b.get(3) - b.get(2)) + b.get(2),
+                    Math.random() * (b.get(5) - b.get(4)) + b.get(4)
+            );
+            if (Space.pointInsidePolyhedron(polyhedron, p)) {
+                within++;
+            }
+        }
+        volume = ((double) within / SAMPLE_SET_FOR_VOLUME_APPROXIMATION) * ((b.get(1) - b.get(0)) * (b.get(3) - b.get(2)) * (b.get(5) - b.get(4)));
+    }
+    
+    /**
+     * Returns the mass of the Polyhedron.
+     *
+     * @return The mass of the Polyhedron.
+     */
+    public double getMass() {
+        return volume * density;
+    }
+    
+    /**
+     * Returns a list of the faces of the Polyhedron.
+     *
+     * @return The list of faces of the Polyhedron.
+     */
+    public List<Polygon> getFaces() {
+        List<Polygon> faces = new ArrayList<>();
+        for (ObjectInterface object : polyhedron.getComponents()) {
+            if (object instanceof Polygon) {
+                faces.add((Polygon) object);
+            }
+        }
+        return faces;
+    }
+    
+    /**
+     * Returns a list of the vertices of the Polyhedron.
+     *
+     * @return The list of vertices of the Polyhedron.
+     */
+    public List<Vector> getVertices() {
+        List<Vector> vertices = new ArrayList<>();
+        List<Polygon> faces = getFaces();
+        for (Polygon face : faces) {
+            vertices.addAll(Arrays.asList(face.getVertices()));
+        }
+        return vertices;
+    }
+    
+    /**
+     * Determines the bounding cube of the Polyhedron.
+     *
+     * @return The bounding cube of the Polyhedron.
+     */
+    private Vector determineBoundingCube() {
+        List<Vector> vertices = getVertices();
+        
+        double xMin = 0, xMax = 0, yMin = 0, yMax = 0, zMin = 0, zMax = 0;
+        for (Vector vertex : vertices) {
+            if (vertex.getX() < xMin) {
+                xMin = vertex.getX();
+            }
+            if (vertex.getX() > xMax) {
+                xMax = vertex.getX();
+            }
+            if (vertex.getY() < yMin) {
+                yMin = vertex.getY();
+            }
+            if (vertex.getY() > yMax) {
+                yMax = vertex.getY();
+            }
+            if (vertex.getZ() < zMin) {
+                zMin = vertex.getZ();
+            }
+            if (vertex.getZ() > zMax) {
+                zMax = vertex.getZ();
+            }
+        }
+        
+        return new Vector(xMin, xMax, yMin, yMax, zMin, zMax);
+    }
+    
+    
+    //Getters
+    
+    /**
+     * Returns the volume of the Polyhedron.
+     *
+     * @return The volume of the Polyhedron.
+     */
+    public double getVolume() {
+        return volume;
+    }
+    
+    /**
+     * Returns the density of the Polyhedron.
+     *
+     * @return The density of the Polyhedron.
+     */
+    public double getDensity() {
+        return density;
+    }
+    
+    
+    //Setters
+    
+    /**
+     * Sets the density of the Polyhedron.
+     * 
+     * @param density The density of the Polyhedron.
+     */
+    public void setDensity(double density) {
+        this.density = density;
+    }
+    
+}
---
+++ src/math/physics/Plane.java
@@ -0,0 +1,76 @@
+/*
+ * File:    Plane.java
+ * Package: math.physics
+ * Author:  Zachary Gill
+ */
+
+package math.physics;
+
+import math.vector.Vector;
+import objects.base.polygon.Polygon;
+
+/**
+ * Includes functions for Planes.
+ */
+public final class Plane {
+    
+    //Functions
+    
+    /**
+     * Determines the point of intersection between a plane defined by a point and a normal vector and a line defined by a point and a direction vector.
+     *
+     * @param planePoint    A point on the plane.
+     * @param planeNormal   The normal vector of the plane.
+     * @param linePoint     A point on the line.
+     * @param lineDirection The direction vector of the line.
+     * @return The point of intersection between the line and the plane, null if the line is parallel to the plane.
+     */
+    public static Vector lineIntersection(Vector planePoint, Vector planeNormal, Vector linePoint, Vector lineDirection) {
+        double t = lineScalarForPointOnPlane(planePoint, planeNormal, linePoint, lineDirection);
+        if (t == Double.MAX_VALUE) {
+            return null;
+        }
+        return linePoint.plus(lineDirection.scale(t));
+    }
+    
+    /**
+     * Determines the line scalar for the intersection between a plane defined by a point and a normal vector and a line defined by a point and a direction vector.
+     *
+     * @param planePoint    A point on the plane.
+     * @param planeNormal   The normal vector of the plane.
+     * @param linePoint     A point on the line.
+     * @param lineDirection The direction vector of the line.
+     * @return The line scalar for the intersection between the line and the plane, Double.MAX_VALUE if the line is parallel to the plane.
+     */
+    public static double lineScalarForPointOnPlane(Vector planePoint, Vector planeNormal, Vector linePoint, Vector lineDirection) {
+        if (planeNormal.dot(lineDirection) == 0) {
+            return Double.MAX_VALUE;
+        }
+        return (planeNormal.dot(planePoint) - planeNormal.dot(linePoint)) / planeNormal.dot(lineDirection);
+    }
+    
+    /**
+     * Determines if a point is inside a Polygon.
+     * 
+     * @param polygon The Polygon.
+     * @param point   The point.
+     * @return Whether the point is inside the Polygon or not.
+     */
+    public static boolean pointInsidePolygon(Polygon polygon, Vector point) {
+        double angleSum = 0;
+        int n = polygon.getVertices().length;
+        for (int i = 0; i < n; i++) {
+            Vector v1 = polygon.getVertex(i + 1).minus(point);
+            Vector v2 = polygon.getVertex((i + 1) % n + 1).minus(point);
+            double d1 = v1.hypotenuse();
+            double d2 = v2.hypotenuse();
+            if (d1 * d2 <= 0.0000001) { //on a vertex
+                return true;
+            } else {
+                angleSum += Math.acos(v1.dot(v2) / (d1 * d2));
+            }
+        }
+        return Math.abs(angleSum - (Math.PI * 2)) <= 0.0000001;
+    }
+    
+}

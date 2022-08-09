6fb2e5a650573a91746894c9f49d48b29b731091
Zachary Gill <zachary.w.gill@gmail.com>
2022-08-09 03:45:00 -0400

    Updated command line script

--- graphy.bat
+++ graphy.bat
@@ -1,6 +1,15 @@
 @echo off
-set type=%1
-set class=%2
-@echo on
 
-mvn compile && mvn exec:java -Dexec.mainClass="graphy.main.%type%.%class%"
+echo Graphy
+
+set mainClass=graphy.main.%1.%2
+set noCompile=false
+set loggerLvl=warn
+set waitAfter=false
+
+mvn compile exec:exec ^
+	-Dorg.slf4j.simpleLogger.defaultLogLevel=%loggerLvl% ^
+	-Dmaven.main.skip="%noCompile%" ^
+	-Dexec.executable="java" ^
+	-Dexec.args="-Dfile.encoding=UTF8 -classpath %%classpath %mainClass%" && ^
+if "%waitAfter%"=="true" (pause)
